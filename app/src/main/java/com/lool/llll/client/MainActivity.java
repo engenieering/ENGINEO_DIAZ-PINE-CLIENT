39baf95f06546d003a9aad8be1bf1d3cecef58ab
Enginero_one_zero <mailyandro@gmail.com>
2016-07-29 23:59:54 +0100

    Initial commit

---
+++ app/src/main/java/com/lool/llll/client/MainActivity.java
@@ -0,0 +1,346 @@
+package com.lool.llll.client;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity implements LocationListener {
+
+    LocationManager locationManager;
+    String provider;
+    static final int SocketServerPORT = 8080;
+
+    LinearLayout loginPanel, chatPanel;
+
+    EditText editTextUserName, editTextAddress;
+    Button buttonConnect;
+    TextView chatMsg, textPort;
+
+    EditText editTextSay;
+    Button buttonSend;
+    Button buttonDisconnect;
+
+    String msgLog = "";
+
+    ChatClientThread chatClientThread = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null){
+            onLocationChanged(location);
+            }else {
+            Toast.makeText(this, "connection failed", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        loginPanel = (LinearLayout)findViewById(R.id.loginpanel);
+        chatPanel = (LinearLayout)findViewById(R.id.chatpanel);
+
+        editTextUserName = (EditText) findViewById(R.id.username);
+        editTextAddress = (EditText) findViewById(R.id.address);
+        textPort = (TextView) findViewById(R.id.port);
+        textPort.setText("port: " + SocketServerPORT);
+        buttonConnect = (Button) findViewById(R.id.connect);
+        buttonDisconnect = (Button) findViewById(R.id.disconnect);
+        chatMsg = (TextView) findViewById(R.id.chatmsg);
+
+        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
+        buttonDisconnect.setOnClickListener(buttonDisconnectOnClickListener);
+
+        editTextSay = (EditText)findViewById(R.id.say);
+        buttonSend = (Button)findViewById(R.id.send);
+
+        buttonSend.setOnClickListener(buttonSendOnClickListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(this);
+
+
+    }
+
+
+
+
+    OnClickListener buttonDisconnectOnClickListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(chatClientThread==null){
+                return;
+            }
+            chatClientThread.disconnect();
+        }
+
+    };
+
+    OnClickListener buttonSendOnClickListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (editTextSay.getText().toString().equals("")) {
+                return;
+            }
+
+            if(chatClientThread==null){
+                return;
+            }
+
+            chatClientThread.sendMsg(editTextSay.getText().toString() + "\n");
+        }
+
+    };
+
+    OnClickListener buttonConnectOnClickListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String textUserName = editTextUserName.getText().toString();
+            if (textUserName.equals("")) {
+                Toast.makeText(MainActivity.this, "Enter User Name",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            String textAddress = editTextAddress.getText().toString();
+            if (textAddress.equals("")) {
+                Toast.makeText(MainActivity.this, "Enter Addresse",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            msgLog = "";
+            chatMsg.setText(msgLog);
+            loginPanel.setVisibility(View.GONE);
+            chatPanel.setVisibility(View.VISIBLE);
+
+            chatClientThread = new ChatClientThread(
+                    textUserName, textAddress, SocketServerPORT);
+            chatClientThread.start();
+        }
+
+    };
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        if ( chatClientThread==null) {
+
+        }
+        else {
+            chatClientThread.sendMsg("lat"+lat+"l"+lng+"lng");
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+
+    private class ChatClientThread extends Thread {
+
+        String name;
+        String dstAddress;
+        int dstPort;
+
+        String msgToSend = "";
+        boolean goOut = false;
+
+        ChatClientThread(String name, String address, int port) {
+            this.name = name;
+            dstAddress = address;
+            dstPort = port;
+        }
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+                dataOutputStream = new DataOutputStream(
+                        socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream.writeUTF(name);
+                dataOutputStream.flush();
+
+                while (!goOut) {
+                    if (dataInputStream.available() > 0) {
+                        msgLog += dataInputStream.readUTF();
+
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                chatMsg.setText(msgLog);
+                            }
+                        });
+                    }
+
+                    if(!msgToSend.equals("")){
+                        dataOutputStream.writeUTF(msgToSend);
+                        dataOutputStream.flush();
+                        msgToSend = "";
+                    }
+                }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                final String eString = e.toString();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, eString, Toast.LENGTH_LONG).show();
+                    }
+
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                final String eString = e.toString();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, eString, Toast.LENGTH_LONG).show();
+                    }
+
+                });
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        loginPanel.setVisibility(View.VISIBLE);
+                        chatPanel.setVisibility(View.GONE);
+                    }
+
+                });
+            }
+
+        }
+
+        private void sendMsg(String msg){
+            msgToSend = msg;
+        }
+
+        private void disconnect(){
+            goOut = true;
+        }
+    }
+
+}
